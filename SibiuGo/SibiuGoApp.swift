a5b67eafbc64f190181166ae5f5a0a3c624f5721
Diana Ciodolan <didi.maria2012@gmail.com>
2026-09-03 16:47:27 +0300

    Initial project setup

---
+++ SibiuGo/SibiuGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SibiuGoApp.swift
+//  SibiuGo
+//
+//  Created by Diana Ciodolan on 03/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SibiuGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

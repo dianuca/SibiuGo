e8a38fe152432f1ec9d9961ca4e91ada9cba595b
Diana Ciodolan <didi.maria2012@gmail.com>
2026-09-03 17:01:16 +0300

    Add initial app structure and tab navigation

--- SibiuGo/App/SibiuGoApp.swift
+++ SibiuGo/App/SibiuGoApp.swift
@@ -1,17 +1,10 @@
-//
-//  SibiuGoApp.swift
-//  SibiuGo
-//
-//  Created by Diana Ciodolan on 03/09/2026.
-//
-
 import SwiftUI
 
 @main
 struct SibiuGoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
---
+++ SibiuGo/Features/ExploreView.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreView.swift
+//  SibiuGo
+//
+//  Created by Diana Ciodolan on 03/09/2026.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Harta Sibiului")
+                .navigationTitle("Explorează")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
